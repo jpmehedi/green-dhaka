059aed6d8ac86dcb62f2aa7cf65ffd54f3c41c62
Mehedi Hasan <mehedihasan.cse.jp@gmail.com>
2020-09-22 23:40:25 +0600

    init

---
+++ android/app/src/main/kotlin/com/mehedihasaninfo/green_dhaka/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mehedihasaninfo.green_dhaka
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: LoginScreen(),
+    );
+  }
+}
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          child: Column(
+            children: <Widget>[
+              Stack(
+                children: <Widget>[Image.asset("assets/image/")],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
